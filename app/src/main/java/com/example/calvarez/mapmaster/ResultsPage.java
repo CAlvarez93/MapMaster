9c072805696342919b723a41dbfd19ee8f194eb6
Alvarez <calvarez@iastate.edu>
2016-11-05 16:26:26 -0500

    adding UI template pages

---
+++ app/src/main/java/com/example/calvarez/mapmaster/ResultsPage.java
@@ -0,0 +1,36 @@
+package com.example.calvarez.mapmaster;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by calvarez on 11/5/2016.
+ */
+public class ResultsPage extends Fragment {
+
+    MainActivity mActivity;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.results_page,container,false);
+
+        return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mActivity = (MainActivity) context;
+    }
+}
