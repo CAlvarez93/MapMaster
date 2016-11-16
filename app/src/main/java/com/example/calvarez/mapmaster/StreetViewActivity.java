b590d32474964869e73b700e573f8100eb2a6f53
Dau <adau@iastate.edu>
2016-11-15 22:18:22 -0600

    added some pretty cool stuff man... gonna be impressed. Created latlng for street views of 10 different locations... yep.

--- app/src/main/java/com/example/calvarez/mapmaster/StreetViewActivity.java
+++ app/src/main/java/com/example/calvarez/mapmaster/StreetViewActivity.java
@@ -26,11 +26,26 @@ import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.StreetViewPanoramaOptions;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class StreetViewActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
 
     int count = 0;
     MainActivity mActivity;
     final Context context = this;
+    final LatLng ames = new LatLng(42.016249,-93.636185);
+    final LatLng chicago = new LatLng(41.8781,-87.6298);
+    final LatLng nyc = new LatLng(40.6892,-74.0445);
+    final LatLng dsm = new LatLng(41.6005,-93.6091);
+    final LatLng london = new LatLng(51.5007,-0.1246);
+    final LatLng paris = new LatLng(48.8584,2.2945);
+    final LatLng saintlouis = new LatLng(38.627003,-90.199404);
+    final LatLng la = new LatLng(34.0522,-118.2437);
+    final LatLng miami = new LatLng(25.7617,-80.1918);
+    final LatLng seattle = new LatLng(47.6205,-122.3493);
+
 
 
 
@@ -53,7 +68,7 @@ public class StreetViewActivity extends FragmentActivity implements OnStreetView
                 // custom dialog
                 final Dialog dialog = new Dialog(context);
                 dialog.setContentView(R.layout.popup);
-                dialog.setTitle("Title...");
+
 
                 Button button = (Button) dialog.findViewById(R.id.dialogButtonOK);
                 RadioButton r1 = (RadioButton) findViewById(R.id.radio1);
@@ -74,9 +89,6 @@ public class StreetViewActivity extends FragmentActivity implements OnStreetView
             }
         });
 
-
-
-
     }
 
 
@@ -84,9 +96,39 @@ public class StreetViewActivity extends FragmentActivity implements OnStreetView
     @Override
     public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
 
-        panorama.setPosition(new LatLng(42.016249,-93.636185));
+        LatLng test[] = new LatLng[10];
+        test[0] = ames;
+        test[1] = chicago;
+        test[2] = dsm;
+        test[3] = la;
+        test[4] = london;
+        test[5] = miami;
+        test[6] = nyc;
+        test[7] = paris;
+        test[8] = saintlouis;
+        test[9] = seattle;
+
+       /* List<LatLng> location = new ArrayList<LatLng>();
+        location.add(ames);
+        location.add(chicago);
+        location.add(nyc);
+        location.add(dsm);
+        location.add(london);
+        location.add(paris);
+        location.add(saintlouis);
+        location.add(la);
+        location.add(miami);
+        location.add(seattle);*/
+
+
+        Random rn = new Random();
+        int answer = rn.nextInt(10);
+
+        panorama.setPosition(test[answer]);
         StreetViewPanoramaOptions options = new StreetViewPanoramaOptions();
         StreetViewPanoramaFragment.newInstance(options);
+
+        Log.i("location", Double.toString(answer));
 
     }
 
